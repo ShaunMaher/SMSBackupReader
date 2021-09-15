503a7b6972dca7acbfd19c141d0d9201f304fa2b
Shaun Maher <shaun@ghanima.net>
2021-09-15 18:44:37 +1000

    Last nights work and a populated README

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
 # SMSBackupReader
+I have an adorable new family member that I get lots and lots of photos of as
+SMS/MMS messages.  I would be horrified if any of these were lost due the a lost
+or damaged phone.
+
+In an attempt to avoid this, I use the great app [SMS Backup & Restore](https://play.google.com/store/apps/details?id=com.riteshsahu.SMSBackupRestore&hl=en_AU&gl=US&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1)
+
+<a href='https://play.google.com/store/apps/details?id=com.riteshsahu.SMSBackupRestore&hl=en_AU&gl=US&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' style='width: 13em; display: block; margin-left: auto; margin-right: auto;' /></a>
+
+**Note**: Neither I personally nor this project are affiliated with the
+developers of "SMS Backup & Restore" or Google
+
+I export the SMS messages from my Android phone to encrypted XML files that are
+uploaded to my Google Drive every evening.
+
+I then have a script that periodically downloads all these files out of my
+Google Drive and removes them from Google.  This is to conserve space on my free
+Google Drive account.
+
+Now we get to the purpose of this project, extracting the content of these
+encrypted XML files.  I would like to:
+* Extract all the images and save them into where I store Photos (and maybe
+  these sync back up to Google Photos)
+* Turn the XML files into something I can read locally, without needing to
+  import the backups back into my phone.  This allows me to have a local archive
+  that I can reference if necessary which in turn lets me delete multiple GiB of
+  MMS messages off my phone.  My phone has reached the point where one message
+  conversation, which dates back several years intermittently crashes the
+  messages app when accessed.  If I KNEW I could access those messages elsewhere
+  I could delete them off my phone.
+
+This project will be (a few?) bash scripts that:
+* Extract all the data out of the encrypted XML files
+* Images are saved to a local directory
+* Messages, with the image data replaced with a reference to an image file, are
+  converted to JSON and saved into an SQLite3 database
+  * Duplicate messages (maybe the same message exists in multiple backups) are
+    automatically ignored
+* Some static HTML files are generated from the database contents
+
+## File Format Information
+The SMS Backup & Restore file format is very simple.  It's a zip compressed file
+and, if you configured a password in the App, the same password can be used to
+extract a simple XML file from the zip file.
+
+Binary content (e.g. Images) are base64 encoded into an element within the XML
+document.
+
+TODO: Add some sanitized examples
+
+## Usage
+The scripts make use of some common *nix utilities which must be installed
+before the script will function.  For Ubuntu:
+```
+sudo apt install jq xmlstarlet python3-pip sqlite3-pcre sqlite3
+sudo pip install yq
+```
+
+### Configuration
+Configuration is (or will be) stored in the SQLite3 database.  To set a
+configuration value:
+```
+extracter.sh config set <name> <value>
+```
+
+The only configuration value at present is your local international dialing code
+so that numbers such as "+61412345678" and "0412345678" can be merged into a
+single contact thread.  To set this value:
+```
+extracter.sh config set my_intl_code 61
+```
+
+I'm sure there will be more configuration values added in the future.
+
+### Parsing XML files
+At this time, I don't have the extraction from zip using a password implemented
+yet so the script expects to be just given an extracted XML file.
+```
+extracter.sh parse /path/to/file.xml
+```
+
+### Generating Static HTML files
+This isn't implemented yet.
+
+## Bash script?
+Yeah, it's not very portable, etc. but I hope that this project will be simple
+enough to have functional within a few days of effort.  Also,
+[Bash is my hammer](https://en.wikipedia.org/wiki/Law_of_the_instrument#Computer_programming).
+
+## Alternatives
+The fine developers of SMS Backup & Restore do provide some other options for
+reading the files they create.  More information can be found here: https://synctech.com.au/sms-backup-restore/view-or-edit-backup-files-on-computer/
+
+Some Notes:
+* "View the backup data using a web browser" - Your browser will struggle if
+  your backup file is several GiB
+* "Threaded view for SMS Messages" - Your browser will still struggle if your
+  backup file is several GiB and, while this method looks prettier, it doesn't
+  seem to support MMS messages.
+* "View/Edit messages using MS Excel" - No thanks. :-)
--- extracter.sh
+++ extracter.sh
@@ -1,27 +1,198 @@
 #!/bin/bash
 
-in_file="./sms-20210911002715.xml"
-tmp_file=$(mktemp)
-cp "${in_file}" "${tmp_file}"
+# Dependencies:
+#  Ubuntu:
+#    jq xmlstarlet python3-pip sqlite3-pcre sqlite3
+#    from pip: yq
 
-mms_count=$(xpath -e "count(/smses/mms)" "${in_file}" 2>/dev/null)
-echo ${mms_count}
-for (( i=0; i<=$mms_count; i++ )); do
-  images=$(xpath -e "count(/smses/mms[$i]/parts/part[@ct=\"image/jpeg\"])" "${in_file}" 2>/dev/null)
-  echo $images
-  for (( p=1; p<=$images; p++ )); do
-    filename=$(mktemp)
-    xpath -q -e "/smses/mms[$i]/parts/part[@ct=\"image/jpeg\"][$p]/@data" "${in_file}" |\
-      sed 's/ data="//g' |\
-      sed 's/"$//g' |\
-      base64 -d >${filename}
-    new_name=$(md5sum ${filename} | awk '{print $1}')".jpeg"
-    if [ ! -f "attachments/${new_name}" ]; then
-      mv "${filename}" "attachments/${new_name}"
-    fi
-    xmlstarlet ed -u "/smses/mms[$i]/parts/part[@ct=\"image/jpeg\"][$p]/@data" -v "${new_name}" "${tmp_file}" >"${tmp_file}.new"
-    mv "${tmp_file}.new" "${tmp_file}"
-  done
+output=1
+
+while getopts "vd" opt; do
+  case $opt in
+    v) output=$(( $output + 1 )) ;;
+    d) output=5 ;;
+    *) usage "-${option} and ${OPTARG}" ;;
+  esac
 done
 
-cp "${tmp_file}" "${in_file}.stripped.xml"
+ns=""
+args=("$@")
+for (( i=1; i<="${#args[@]}"; i++ )); do
+  shift
+  case "${args[$i]}" in
+    "parse")
+      ns="parse"
+      in_file=${args[$i + 1]}
+      if [ "${in_file}" == "" ]; then
+        echo "You didn't specify a file to parse."
+      fi
+      shift
+      ;;
+    "config") ns="config" ;;
+    "generate") ns="generate" ;;
+    "set")
+      if [ "${ns}" == "config" ]; then
+        ns="config_set"
+        name=${args[$i + 1]}
+        value=${args[$i + 2]}
+        shift 2
+        #echo "update config database: set ${name} ${value}"
+      fi
+      ;;
+    "get")
+      if [ "${ns}" == "config" ]; then
+        ns="config_get"
+        name=${args[$i + 1]}
+        shift
+        #echo "query config database: get ${name}"
+      fi
+      ;;
+    "clear")
+      if [ "${ns}" == "config" ]; then
+        ns="config_clear"
+        #echo "query config database: get ${name}"
+      fi
+      ;;
+    "html")
+      if [ "${ns}" == "generate" ]; then
+        ns="generate_html"
+        #echo "query config database: get ${name}"
+      fi
+      ;;
+    *) set -- "$@" "$arg"
+  esac
+done
+
+config_get () {
+  sql_query="SELECT value FROM config WHERE ((name = '"$1"') AND (version = (SELECT MAX(version) AS version FROM config WHERE name = '"$1"')))"
+  if [ $output -gt 3 ]; then echo "${sql_query}" 1>&2; fi
+  return=$(echo "${sql_query}" | sqlite3 "${database_file}")
+  if [ $output -gt 3 ]; then echo "${sql_query}: ${return}" 1>&2; fi
+  echo "${return}"
+}
+
+config_set () {
+  sql_query="INSERT INTO config VALUES('"$(date +%s)"', '"$1"', '"$2"');"
+  if [ $output -gt 3 ]; then echo "${sql_query}" 1>&2; fi
+  echo "${sql_query}" | sqlite3 "${database_file}"
+}
+
+config_clear () {
+  sql_query="DELETE FROM config;"
+  if [ $output -gt 3 ]; then echo "${sql_query}" 1>&2; fi
+  echo "${sql_query}" | sqlite3 "${database_file}"
+}
+
+config_quick () {
+  true
+}
+
+attachments_dir="./example-data/attachments/"
+database_file="./data/temp"
+if [ ! -f "${database_file}" ]; then
+  echo "An existing database was not found.  One will be created."
+  echo "CREATE TABLE messages(message_time INTEGER, extraction_time INTEGER, sender TEXT, message_json TEXT)" | \
+    sqlite3 "${database_file}"
+  echo "CREATE TABLE config(version INTEGER, name TEXT, value TEXT)" | \
+    sqlite3 "${database_file}"
+  echo "CREATE TABLE state(version INTEGER, name TEXT, value TEXT)" | \
+    sqlite3 "${database_file}"
+fi
+
+my_intl_code=$(config_get "my_intl_code")
+if [ "${my_intl_code}" == "" ]; then
+  echo "You should set your international dialing code to help normalize the numbers displayed."
+  echo "For example, if you live in Australia, run the command \"<TODO:my cmd name> config set my_intl_code 61\""
+  my_intl_code="61"
+fi
+
+normalize_number () {
+  return=$(echo "${1}" | sed "s/\\+${my_intl_code}/0/g")
+  if [ $output -gt 2 ]; then echo "Normalized number: '${1}' -> '${return}'" 1>&2; fi
+  echo "${return}"
+}
+
+parse () {
+  echo "Parsing file ${in_file}"
+  sms_count=$(cat "${in_file}" | xmlstarlet sel -t -v "count(/smses/sms)" 2>/dev/null)
+  echo "Processing $sms_count SMS messages"
+  for (( i=1; i<=$sms_count; i++ )); do
+    message=$(cat "${in_file}" | xmlstarlet sel -t -c "/smses/sms[$i]" 2>/dev/null)
+    safe_json=$(printf "${message}" | xq | sed "s/'/\&#39;/g")
+    message_date=$(echo "${safe_json}" | jq -r '.sms["@date"]' | awk '{print int($1/1000)}')
+    sender=$(echo "${safe_json}" | jq -r '.sms["@address"]')
+    sender=$(normalize_number "${sender}")
+    sql_insert="INSERT INTO messages VALUES (${message_date}, "$(date +%s)", '${sender}', '${safe_json}');"
+    sql_count="SELECT COUNT(*) FROM messages WHERE HEX(SHA3(message_json)) = HEX(SHA3('${safe_json}'))"
+    existing_count=$(echo "${sql_count}" | sqlite3 "${database_file}")
+    if [ $existing_count -lt 1 ]; then
+      echo "${sql_insert}" | sqlite3 "${database_file}"
+    else
+      echo "  Not inserting duplicate message into database"
+    fi
+  done
+
+  mms_count=$(cat "${in_file}" | xmlstarlet sel -t -v "count(/smses/mms)" "${in_file}" 2>/dev/null)
+  echo "Processing ${mms_count} MMS messages"
+  for (( i=1; i<=$mms_count; i++ )); do
+    message=$(cat "${in_file}" | xmlstarlet sel -t -c "/smses/mms[$i]" 2>/dev/null)
+    images=$(echo "${message}" | xmlstarlet sel -t -v "count(/mms/parts/part[@ct=\"image/jpeg\"])" 2>/dev/null)
+    echo "  Extracting $images image(s) from message"
+    for (( p=1; p<=$images; p++ )); do
+      filename=$(mktemp)
+      echo "${message}" | \
+        xmlstarlet sel -t -v "/mms/parts/part[@ct=\"image/jpeg\"][$p]/@data" | \
+        base64 -d >${filename}
+      new_name=$(md5sum ${filename} | awk '{print $1}')".jpeg"
+      if [ ! -f "attachments/${new_name}" ]; then
+        mv "${filename}" "${attachments_dir}/${new_name}"
+      fi
+      safe_json=$(
+        echo "${message}" | \
+        xmlstarlet ed -u "/mms/parts/part[@ct=\"image/jpeg\"][$p]/@data" -v "${new_name}" | \
+        xq | \
+        sed "s/'/\&#39;/g"
+      )
+      message_date=$(echo "${safe_json}" | jq -r '.mms["@date"]' | awk '{print int($1/1000)}')
+      sender=$(echo "${safe_json}" | jq -r '.mms["@address"]')
+      sender=$(normalize_number "${sender}")
+      sql_count="SELECT COUNT(*) FROM messages WHERE HEX(SHA3(message_json)) = HEX(SHA3('${safe_json}'))"
+      sql_insert="INSERT INTO messages VALUES (${message_date}, "$(date +%s)", '${sender}', '${safe_json}');"
+      existing_count=$(echo "${sql_count}" | sqlite3 "${database_file}")
+      if [ $existing_count -lt 1 ]; then
+        echo "${sql_insert}" | sqlite3 "${database_file}"
+      else
+        echo "  Not inserting duplicate message into database"
+      fi
+    done
+  done
+}
+
+generate_html () {
+  # To get a list of unique senders
+  # SELECT DISTINCT(COALESCE(json_extract(message_json, '$.mms.@address'), COALESCE(json_extract(message_json, '$.sms.@address'), ''))) as sender FROM messages;
+  #sql_query="SELECT DISTINCT(COALESCE(json_extract(message_json, '\$.mms.@address'), COALESCE(json_extract(message_json, '\$.sms.@address'), ''))) as sender FROM messages;"
+  sql_query="SELECT DISTINCT(sender) as sender FROM messages;"
+  while read sender; do
+    echo "Generating a view for sender ${sender}"
+  done < <(echo "${sql_query}" | sqlite3 "${database_file}")
+
+  sql_query="SELECT MIN(message_time) as max FROM messages;"
+  oldest=$(echo "${sql_query}" | sqlite3 "${database_file}")
+  sql_query="SELECT MAX(message_time) as max FROM messages;"
+  newest=$(echo "${sql_query}" | sqlite3 "${database_file}")
+  # A lazy way to generate a list of months between the oldest known message and
+  #  the newest.  Start at the oldest date, add one day at a time and have the
+  #  "date" command report what month that was.  Then dedup the list.
+  for (( i=$oldest; i<=$newest; i+=3600 )); do
+    echo "$i "$(date --date="@${i}" "+%B %Y")
+  done
+}
+
+case "${ns}" in
+  parse) parse ;;
+  generate_html) generate_html ;;
+  config_set) config_set "${name}" "${value}" ;;
+  config_get) config_get "${name}" ;;
+  config_clear) config_clear ;;
+esac
